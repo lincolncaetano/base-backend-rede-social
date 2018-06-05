17da741ccda744907ed989373608ec4893cfbaae
Lincoln Caetano <lincolncaetano@gmail.com>
2018-06-05 03:32:33 -0300

    att security

---
+++ src/main/java/br/com/dlcstudio/base/repositories/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package br.com.dlcstudio.base.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.dlcstudio.base.domain.Usuario;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+	
+}
--- src/main/java/br/com/dlcstudio/base/services/DBService.java
+++ src/main/java/br/com/dlcstudio/base/services/DBService.java
@@ -1,15 +1,30 @@
 package br.com.dlcstudio.base.services;
 
 import java.text.ParseException;
+import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import br.com.dlcstudio.base.domain.Usuario;
+import br.com.dlcstudio.base.repositories.UsuarioRepository;
 
 @Service
 public class DBService {
+	
+	@Autowired
+	private BCryptPasswordEncoder pe;
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
 
 	public void instantiateTestDatabase() throws ParseException {
 		
-		
+		Usuario user = new Usuario(null, "lincolncaetano", pe.encode("123"));
+		Usuario user2 = new Usuario(null, "teste", pe.encode("123"));
+		Usuario user3 = new Usuario(null, "teste2", pe.encode("123"));
+		usuarioRepository.saveAll(Arrays.asList(user, user2, user3));
 			
 	}
 }
