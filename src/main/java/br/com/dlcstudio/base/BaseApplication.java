12fd927b0139010ac7ed5b463cc2ce516f0a9838
Lincoln Caetano <lincolncaetano@gmail.com>
2018-06-05 03:22:52 -0300

    atualizando profiles

--- src/main/java/br/com/dlcstudio/base/BaseApplication.java
+++ src/main/java/br/com/dlcstudio/base/BaseApplication.java
@@ -1,12 +1,17 @@
 package br.com.dlcstudio.base;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BaseApplication {
+public class BaseApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BaseApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {	
 	}
 }
---
+++ src/main/java/br/com/dlcstudio/base/services/DBService.java
@@ -0,0 +1,15 @@
+package br.com.dlcstudio.base.services;
+
+import java.text.ParseException;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DBService {
+
+	public void instantiateTestDatabase() throws ParseException {
+		
+		
+			
+	}
+}
